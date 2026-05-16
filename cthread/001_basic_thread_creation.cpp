f8088c8e64b8d9428448037f5a79d3d38fd0462f
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-05-16 22:03:26 +0600

    thread-basic

---
+++ cthread/001_basic_thread_creation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<pthread.h>
+using namespace std;
+
+
+
+
+void* show_message(void* arg){
+    cout<<"Thread is running"<<endl;
+    cout<<"Thread ID: ";
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
